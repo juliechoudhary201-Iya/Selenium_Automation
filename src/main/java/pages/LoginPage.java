d2de0d9a26cc4ea3d792d62961221b5e4fed95e1
Julie Choudhary <juliechoudhary201@gmail.com>
2025-04-10 20:54:12 +0530

    Data Driven Code

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import utils.Log;
 
@@ -9,29 +11,50 @@ public class LoginPage {
 	
 	private WebDriver driver;
 	
-	private By Username = By.id("Email");
-	private By Password = By.id("Password");
-	private By Login = By.xpath("//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button");
+	//Defined for the Page Factory
+	@FindBy(id="Email")
+	WebElement Username;
+	
+	@FindBy(id="Password")
+	WebElement Password;
+	
+	//@FindBy(xpath="//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[4]/button")
+	@FindBy(css = "button[type='submit']")
+	WebElement Login;
+	
+	
+	//private By Username = By.id("Email"); ......USED IN POM
+	//private By Password = By.id("Password");......USED IN POM
+	//private By Login = By.xpath("//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button"); ......USED IN POM
 	
 	public LoginPage(WebDriver driver)
 	{
 		this.driver =driver; 
+		PageFactory.initElements(driver, this); //Defined for the Page Factory
 	}
 
 	public void Username(String username)
 	{
-		driver.findElement(Username).clear();
-		driver.findElement(Username).sendKeys(username);
+		Username.clear();   //Defined for the Page Factory
+		Username.sendKeys(username);   //Defined for the Page Factory
+		
+		//driver.findElement(Username).clear(); ......USED IN POM
+		//driver.findElement(Username).sendKeys(username); ......USED IN POM
 	}
 	public void Password(String password)
 	{
-		driver.findElement(Password).clear();
-		driver.findElement(Password).sendKeys(password);
+		Password.clear(); //Defined for the Page Factory
+		Password.sendKeys(password); //Defined for the Page Factory
+		
+		//driver.findElement(Password).clear(); ......USED IN POM
+		//driver.findElement(Password).sendKeys(password); ......USED IN POM
 	}
 	public void Login()
 	{
 		Log.info("Cicking Login button...");
-		driver.findElement(Login).click();
+		Login.click(); //Defined for the Page Factory
+		
+		//driver.findElement(Login).click(); ......USED IN POM
 		
 	}
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,19 +1,51 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtils;
 import utils.ExtentReportManager;
 import utils.Log;
 
-public class LoginTest extends BaseTest{
+public class LoginTest extends BaseTest {
+
+	@DataProvider(name = "LoginData1") // To get DATA from Excel Sheet
+	public Object[][] getLoginData() throws IOException {
+
+		String filePath = System.getProperty("user.dir") + "/TestData/TestData.xlsx";
+		ExcelUtils.loadExcel(filePath, "Sheet1");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount - 1][2];
+
+		for (int i = 1; i < rowCount; i++) {
+
+			data[i - 1][0] = ExcelUtils.getCellData(i, 0); // Username
+			data[i - 1][1] = ExcelUtils.getCellData(i, 1); // Password
+		}
+		ExcelUtils.closeExcel();
+		return data;
+	}
+
+	//@DataProvider(name = "LoginData2") ..........To get DATA from Data Provider Method
+	//public Object[][] getData() {
+
+		//return new Object[][] { { "user1", "pass1" }, { "user2", "pass2" }, { "user3", "pass3" } };
+	//}
+
 	
-	@Test
-	public void validLogin()
-	{
-			
+	@Test(dataProvider = "LoginData1") // Here we can call the dataProvider name either "LoginData1" OR "LoginData2"
+
+	// @Test
+	// @Parameters({"username","password"}) .......To get DATA from testngData.xml
+
+	public void validLogin(String username, String password) {
+
 		Log.info("Starting login test...");
 		test = ExtentReportManager.createTest("Login Test - ");
 
@@ -22,18 +54,25 @@ public class LoginTest extends BaseTest{
 
 		Log.info("Adding credentials");
 		test.info("Adding Credentails");
-		loginPage.Username("admin@yourstore.com");
-		loginPage.Password("admin");
-//		loginPage.enterUsername(username);
-//		loginPage.enterPassword(password);
+
+		// loginPage.Username("admin@yourstore.com");
+		// loginPage.Password("admin");
+
+		loginPage.Username(username);
+		loginPage.Password(password);
+
 		test.info("Clicking on Login button");
 		loginPage.Login();
 
 		System.out.println("Title of the page is : " + driver.getTitle());
 		Log.info("Verifying page title");
 		test.info("Verifying page title");
-		Assert.assertEquals(driver.getTitle(), "Just a moment...1234");
+		// Get the title of the page
+		String pageTitle = driver.getTitle();
 
+		// Assert that the title contains the expected substring
+		Assert.assertTrue(pageTitle.contains("Just a moment..."),
+				"Page title does not contain the expected substring.");
 		test.pass("Login Successful");
 	}
 }
